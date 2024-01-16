2dc2973b414e5fd5c492a46883fbdf290a3b63ba
Light-Lens <QCoreNest@gmail.com>
2024-01-17 00:22:26 +0530

    Remove data folder

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-# from src.w2.train import train
+from src.w2.train import train
 from src.w2.sample import sample
 from src.w2.utils import dprint
 
-# # Train the model.
-# T1 = train(
-#     batch_size = 16,
-#     block_size = 64,
-#     lr = 1e-3,
-#     n_embd = 64,
-#     n_layer = 6,
-#     n_head = 6,
-#     dropout = 0
-# )
+# Train the model.
+T1 = train(
+    batch_size = 16,
+    block_size = 1024,
+    lr = 3e-4,
+    n_embd = 128,
+    n_layer = 8,
+    n_head = 8,
+    dropout = 0
+)
 
-# T1.preprocess_data("data\\data_small.txt")
-# T1.train(
-#     n_steps = 10000,
-#     eval_interval = 1000,
-#     eval_iters = 200
-# )
+T1.preprocess_data("data\\data.txt")
+T1.train(
+    n_steps = 300000,
+    eval_interval = 2000,
+    eval_iters = 200
+)
 
-# T1.save("models\\GAT-w2.pth")
+T1.save("models\\GAT-w2.pth")
 
 # Use the model
 S1 = sample("models\\GAT-w2.pth")
@@ -28,7 +28,10 @@ S1.load()
 
 while True:
     inp = input("> ")
-    if inp == "q" or inp == "bye":
+    if inp == "":
+        continue
+
+    elif inp == "q" or inp == "bye":
         break
 
-    dprint(S1.generate(f"Human 1: {inp}\nHuman 2: "))
+    dprint(S1.generate(inp, length=100))
