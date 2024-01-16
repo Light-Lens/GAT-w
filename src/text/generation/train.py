44ed2c22150f74dcbeebfa136e1270acb4ff9e2c
Light-Lens <QCoreNest@gmail.com>
2024-01-16 10:48:50 +0530

    Update train.py

--- src/text/generation/train.py
+++ src/text/generation/train.py
@@ -1,5 +1,5 @@
-from src.text.generation.utils import encode, decode
 from src.text.generation.model import GPT, set_params
+from src.text.generation.utils import encode
 import torch, time
 
 # hyperparameters
