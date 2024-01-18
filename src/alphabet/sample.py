cc70f0546ed2bfe6675acdf005f37c175a5c37e8
Light-Lens <QCoreNest@gmail.com>
2024-01-19 04:17:55 +0530

    Update

---
+++ src/alphabet/sample.py
@@ -0,0 +1,10 @@
+class sample:
+    def __init__(self, model_path):
+        pass
+
+    def load(self):
+        pass
+
+    # Use the model for generation or other tasks
+    def classify(self):
+        pass
---
+++ src/alphabet/train.py
@@ -0,0 +1,12 @@
+class train:
+    def __init__(self):
+        pass
+
+    def preprocess(self):
+        pass
+
+    def train(self):
+        pass
+
+    def save(self):
+        pass
