cfa71104dfc649607024a5586c9fd94e1c31ecfe
Light-Lens <QCoreNest@gmail.com>
2024-01-18 11:13:48 +0530

    Remove dprint from utils.py

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 from src.write.train import train
 from src.write.sample import sample
-from src.write.utils import dprint
+import time
+
+# ChatGPT like print effect.
+# dprint -> delay print
+def dprint(text, delay=0.001):
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(delay)
+    print()
 
 # Train the model.
 T1 = train(
-    batch_size = 16,
+    batch_size = 64,
     block_size = 50,
-    lr = 1e-2,
+    lr = 2e-3,
     n_embd = 16,
-    n_layer = 3,
-    n_head = 3,
-    dropout = 0.2
+    n_layer = 4,
+    n_head = 4,
+    dropout = 0
 )
 
-T1.preprocess_data("data\\data_chatgpt.txt", 0.9)
+T1.preprocess_data("data\\data_small.txt", 0.9)
 T1.train(
     n_steps = 5000,
     eval_interval = 1000,
     eval_iters = 200
 )
 
-T1.save("models\\GAT-w2_light.pth")
+T1.save("models\\GAT-w2_chat.pth")
 
 # Use the model
-S1 = sample("models\\GAT-w2_light.pth")
+S1 = sample("models\\GAT-w2_chat.pth")
 S1.load()
 
 while True:
--- src/write/utils.py
+++ src/write/utils.py
@@ -1,13 +1,3 @@
-import time
-
-# ChatGPT like print effect.
-# dprint -> delay print
-def dprint(text, delay=0.001):
-    for char in text:
-        print(char, end="", flush=True)
-        time.sleep(delay)
-    print()
-
 # encoder: take a string, output a list of integers
 def encode(text, stoi):
     return [stoi[c] for c in text]
