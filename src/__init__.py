237dbc7b2aad3b02b561368a58d3c1bbe9fc7419
Light-Lens <QCoreNest@gmail.com>
2024-03-18 11:47:32 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from alphabet import classify, extract
 from models import FeedForwardConfig, FeedForward, RNNConfig, RNN, GPTConfig, GPT
-from write import Sample, Train
+import write as write
 import utils as utils
