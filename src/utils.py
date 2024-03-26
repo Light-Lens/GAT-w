60beb1b0167ddf53bb6b98b549bb78642d888632
Light-Lens <QCoreNest@gmail.com>
2024-03-27 01:21:31 +0530

    Move char level encoder decoder to tokenizer folder

--- src/utils.py
+++ src/utils.py
@@ -23,11 +23,3 @@ def one_hot_encoding(tokenized_sentence, words):
             encoding[idx] = 1
 
     return encoding
-
-# encoder: take a string, output a list of integers
-def encode(text, stoi):
-    return [stoi[c] for c in text]
-
-# decoder: take a list of integers, output a string
-def decode(tensor, itos):
-    return ''.join([itos[i] for i in tensor])
