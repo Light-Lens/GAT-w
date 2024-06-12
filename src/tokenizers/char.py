b038565da46e3378c170bfa6615285f19221deb4
SrijanSriv211 <QCoreNest@gmail.com>
2024-06-12 14:53:55 +0530

    Update char.py

--- src/tokenizers/char.py
+++ src/tokenizers/char.py
@@ -1,8 +1,20 @@
 class CharTokenizer:
+    def __init__(self):
+        pass
+
+    def train(self, text):
+        # here are all the unique characters that occur in this text
+        chars = sorted(list(set(text)))
+        # create a mapping from characters to integers
+        stoi = { ch:i for i,ch in enumerate(chars) }
+        itos = { i:ch for i,ch in enumerate(chars) }
+
+        return stoi, itos
+
     # encoder: take a string, output a list of integers
-    def encode(text, stoi):
+    def encode(self, text, stoi):
         return [stoi[c] for c in text]
 
     # decoder: take a list of integers, output a string
-    def decode(tensor, itos):
+    def decode(self, tensor, itos):
         return ''.join([itos[i] for i in tensor])
